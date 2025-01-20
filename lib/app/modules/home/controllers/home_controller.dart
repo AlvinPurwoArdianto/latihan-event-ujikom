a884fe39255adc58fc8123b6952380f4d3bee31a
alvin <alvinpurwo03@gmail.com>
2025-01-20 12:01:40 +0700

    first commit

---
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:get/get.dart';
+import 'package:latihan_event/app/modules/login/views/login_view.dart';
+
+class HomeController extends GetxController {
+  late Timer _pindah;
+
+  @override
+  void onInit() {
+    _pindah = Timer.periodic(
+      const Duration(seconds: 4),
+      (timer) => Get.off(
+        () => const LoginView(),
+        transition: Transition.leftToRight,
+      ),
+    );
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    _pindah.cancel();
+    super.onClose();
+  }
+}
