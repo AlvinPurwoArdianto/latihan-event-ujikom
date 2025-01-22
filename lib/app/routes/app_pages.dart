89eb34f61a302e168dc60177fea5c3b34801ca2f
alvin <alvinpurwo03@gmail.com>
2025-01-22 09:26:29 +0700

    Tugas Tambah Profile

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,14 +1,17 @@
 import 'package:get/get.dart';
-// import 'package:latihan_event/app/modules/register/views/register_view.dart';
 
 import '../modules/dashboard/bindings/dashboard_binding.dart';
 import '../modules/dashboard/views/dashboard_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
-import '../modules/register/views/register_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
+import '../modules/profile/bindings/profile_binding.dart';
+import '../modules/profile/views/profile_view.dart';
 import '../modules/register/bindings/register_binding.dart';
+import '../modules/register/views/register_view.dart';
+
+// import 'package:latihan_event/app/modules/register/views/register_view.dart';
 
 part 'app_routes.dart';
 
@@ -37,6 +40,11 @@ class AppPages {
       name: _Paths.DASHBOARD,
       page: () => DashboardView(),
       binding: DashboardBinding(),
+    ),
+    GetPage(
+      name: _Paths.PROFILE,
+      page: () => const ProfileView(),
+      binding: ProfileBinding(),
     ),
   ];
 }
